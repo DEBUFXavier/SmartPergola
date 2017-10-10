0fff2560d8fed13dcec1a7cfa5f18d21f91d32b2
Corentin Ballot <corentin.ballot@gmail.com>
2017-10-10 11:37:56 +0200

    add function createMaterial and createGround

--- draw.js
+++ draw.js
@@ -1,45 +1,59 @@
 window.addEventListener('DOMContentLoaded', function() {
+
     var canvas = document.getElementById('renderCanvas');
     var engine = new BABYLON.Engine(canvas, true);
-console.log('in');
-var createScene = function() {
-    // create a basic BJS Scene object
-    var scene = new BABYLON.Scene(engine);
+    console.log('in');
 
-    // create a FreeCamera, and set its position to (x:0, y:5, z:-10)
-    var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5,-10), scene);
+    var createScene = function() {
+        // create a basic BJS Scene object
+        var scene = new BABYLON.Scene(engine);
 
-    // target the camera to scene origin
-    camera.setTarget(BABYLON.Vector3.Zero());
+        // create a FreeCamera, and set its position to (x:0, y:5, z:-10)
+        var camera = new BABYLON.FreeCamera('camera_1', new BABYLON.Vector3(0, 5,-10), scene);
+        // target the camera to scene origin
+        camera.setTarget(BABYLON.Vector3.Zero());
+        // attach the camera to the canvas
+        camera.attachControl(canvas, false);
 
-    // attach the camera to the canvas
-    camera.attachControl(canvas, false);
+        // create a basic light, aiming 0,1,0 - meaning, to the sky
+        var light = new BABYLON.HemisphericLight('light_1', new BABYLON.Vector3(0,1,0), scene);
 
-    // create a basic light, aiming 0,1,0 - meaning, to the sky
-    var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
+        // return the created scene
+        return scene;
+    }
 
-    // create a built-in "sphere" shape; its constructor takes 4 params: name, subdivisions, radius, scene
-    var sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene);
+    var createMaterial = function(scene) {
+        var material = {
+            "grass": new BABYLON.StandardMaterial('grass', scene),
+        }
 
-    // move the sphere upward 1/2 of its height
-    sphere.position.y = 1;
+        material.grass.diffuseTexture = new BABYLON.Texture("img/textures/grass.jpg", scene);
+        material.grass.diffuseTexture.vScale = 50.0;
+        material.grass.diffuseTexture.uScale = 50.0;
 
-    // create a built-in "ground" shape; its constructor takes 5 params: name, width, height, subdivisions and scene
-    var ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene);
+        return material;
+    }
 
-    // return the created scene
-    return scene;
-}
-// call the createScene function
-           var scene = createScene();
+    var createGround = function(scene) {
+        // create a built-in "ground" shape; its constructor takes 5 params: name, width, height, subdivisions and scene
+        var ground = BABYLON.Mesh.CreateGround('ground_1', 60, 60, 2, scene);
+        ground.material = material.grass;
+    }
 
-           // run the render loop
-           engine.runRenderLoop(function(){
-               scene.render();
-           });
+    // call the createScene function
+    var scene = createScene();
 
-           // the canvas/window resize event handler
-           window.addEventListener('resize', function(){
-               engine.resize();
-           });
+    var material = createMaterial(scene);
+
+    createGround(scene);
+
+    // run the render loop
+    engine.runRenderLoop(function(){
+        scene.render();
+    });
+
+    // the canvas/window resize event handler
+    window.addEventListener('resize', function(){
+        engine.resize();
+    });
 });
