48eb83dde783a7c0daceb576050825165f39c9c5
Corentin Ballot <corentin.ballot@gmail.com>
2017-10-10 14:31:13 +0200

    add function createConservatory and armature creation

--- draw.js
+++ draw.js
@@ -9,11 +9,16 @@ window.addEventListener('DOMContentLoaded', function() {
         var scene = new BABYLON.Scene(engine);
 
         // create a FreeCamera, and set its position to (x:0, y:5, z:-10)
-        var camera = new BABYLON.FreeCamera('camera_1', new BABYLON.Vector3(0, 5,-10), scene);
+        //var camera = new BABYLON.FreeCamera('camera_1', new BABYLON.Vector3(0, 5,-10), scene);
+        // Parameters : name, alpha, beta, radius, target, scene
+        var camera = new BABYLON.ArcRotateCamera("ArcRotateCamera", 0.5, 0.5, 10, new BABYLON.Vector3(0, 0, 0), scene);
+
         // target the camera to scene origin
         camera.setTarget(BABYLON.Vector3.Zero());
         // attach the camera to the canvas
         camera.attachControl(canvas, false);
+        // set zoom on scroll more accurate
+        camera.wheelPrecision = 50; 
 
         // create a basic light, aiming 0,1,0 - meaning, to the sky
         var light = new BABYLON.HemisphericLight('light_1', new BABYLON.Vector3(0,1,0), scene);
@@ -40,12 +45,63 @@ window.addEventListener('DOMContentLoaded', function() {
         ground.material = material.grass;
     }
 
+    var createConservatory = function(scene) {
+        // Let's try our built-in 'sphere' shape. Params: name, subdivisions, size, scene
+        var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
+
+        // Move the sphere upward 1/2 its height
+        sphere.position.y = 1;
+
+        // create armature
+        var armature = [
+            // 4 pieds
+            {"sx": 0.1, "sy": 2.5, "sz": 0.1, "px": 3.0, "py": 2.5/2.0, "pz": 0.0},
+            {"sx": 0.1, "sy": 2.5, "sz": 0.1, "px": 3.0, "py": 2.5/2.0, "pz": 3.0},
+            {"sx": 0.1, "sy": 2.5, "sz": 0.1, "px":-3.0, "py": 2.5/2.0, "pz": 0.0},
+            {"sx": 0.1, "sy": 2.5, "sz": 0.1, "px":-3.0, "py": 2.5/2.0, "pz": 3.0},
+            // 
+            {"sx": 6.0, "sy": 0.1, "sz": 0.1, "px": 0.0, "py": 2.5-0.1/2.0, "pz": 3.0},
+            {"sx": 6.0, "sy": 0.1, "sz": 0.1, "px": 0.0, "py": 2.5-0.1/2.0, "pz": 0.0},
+            {"sx": 6.0, "sy": 0.1, "sz": 0.1, "px": 0.0, "py": 0.0+0.1/2.0, "pz": 3.0},
+            {"sx": 6.0, "sy": 0.1, "sz": 0.1, "px": 0.0, "py": 0.0+0.1/2.0, "pz": 0.0},
+            //
+            {"sx": 0.1, "sy": 0.1, "sz": 3.0, "px": 3.0, "py": 2.5-0.1/2.0, "pz": 3.0/2.0},
+            {"sx": 0.1, "sy": 0.1, "sz": 3.0, "px": 3.0, "py": 0.0+0.1/2.0, "pz": 3.0/2.0},
+            {"sx": 0.1, "sy": 0.1, "sz": 3.0, "px":-3.0, "py": 2.5-0.1/2.0, "pz": 3.0/2.0},
+            {"sx": 0.1, "sy": 0.1, "sz": 3.0, "px":-3.0, "py": 0.0+0.1/2.0, "pz": 3.0/2.0},
+        ];
+
+        armature.forEach(function(e, i){
+            var cube = BABYLON.Mesh.CreateBox('consercatory_armature_' + i, 1, scene);
+            cube.scaling.x = e.sx;
+            cube.scaling.y = e.sy;
+            cube.scaling.z = e.sz;
+            cube.position.x = e.px;
+            cube.position.y = e.py;
+            cube.position.z = e.pz;
+        });
+
+
+        //murs pour le store
+        var triangle1 = [ 
+            new BABYLON.Vector3(0,-10,0),
+            new BABYLON.Vector3(-70,-70,0),
+            new BABYLON.Vector3(-70,-105,0),
+        ];
+        var triangle2 = [
+            new BABYLON.Vector3(0,10,0),
+            new BABYLON.Vector3(-70,70,0),
+            new BABYLON.Vector3(-70,105,0),
+        ];
+    }
+
     // call the createScene function
     var scene = createScene();
 
     var material = createMaterial(scene);
 
     createGround(scene);
+    createConservatory(scene);
 
     // run the render loop
     engine.runRenderLoop(function(){
